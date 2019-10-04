ea0b6de7e98be3683bb4e5fd4405b2e803dde3e7
msong-stratx <msong@stratx.co>
2019-10-04 15:27:53 -0400

    remove unnecessary double pointers

--- Data Structures/Linked Lists/reverse_linked_list.c
+++ Data Structures/Linked Lists/reverse_linked_list.c
@@ -5,18 +5,18 @@
 
 #include "./header_files/linked_list.h"
 
-void reverse_linked_list(Node **head, Node **prev, Node **curr)
+void reverse_linked_list(Node **head, Node *prev, Node *curr)
 {
     if ((*head) == NULL || (*head)->next == NULL) return;
 
-    if ((*curr)->next == NULL) {
-        *head = *curr;  // Doesn't affect prev since prev is at a different address than head
-        (*curr)->next = *prev;
-        (*prev)->next = NULL;
+    if (curr->next == NULL) {
+        *head = curr;  // Doesn't affect prev since prev is at a different address than head
+        curr->next = prev;
+        prev->next = NULL;
     } else {
-        reverse_linked_list(&(*head),&(*prev)->next, &(*curr)->next);
-        (*curr)->next = *prev;
-        (*prev)->next = NULL;
+        reverse_linked_list(&(*head),prev->next, curr->next);
+        curr->next = prev;
+        prev->next = NULL;
     }
 }
 
@@ -25,13 +25,13 @@ int main (void)
 {
     srand(time(0));
 
-    int num_vals = rand() % 11;
+    int num_vals = (rand() % 11) + 1;
 
     Node *head = init_rand_linked_list(num_vals);
     Node *prev = head;  
 
     print_linked_list(head);
-    reverse_linked_list(&head,&prev,&head->next);
+    reverse_linked_list(&head,prev,head->next);
     print_linked_list(head);
 
     return 0;
