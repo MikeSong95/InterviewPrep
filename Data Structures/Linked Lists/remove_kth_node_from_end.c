1ef1eb87533c615b83d8009d2eeebed14328e169
msong-stratx <msong@stratx.co>
2019-10-04 17:55:07 -0400

    remove kth node from end implementation

---
+++ Data Structures/Linked Lists/remove_kth_node_from_end.c
@@ -0,0 +1,57 @@
+
+#include "./header_files/linked_list.h"
+
+void remove_kth_node_from_end(Node **head, int k)
+{
+    if ((*head) == NULL) return;            // head is empty
+    if ((*head)->next == NULL && k == 1) {  // one node in list
+        free((*head));
+        head = NULL;
+        return;
+    } 
+
+    Node *prev;
+    Node *main = *head;
+    Node *ref = *head;
+
+    for (; k > 0; k--)
+    {
+        ref = ref->next;
+    }
+
+    if (ref == NULL) {          // remove first node
+        main = main->next;
+        free((*head));
+        *head = NULL;
+        *head = main;
+    } else {
+        while(ref != NULL)
+        {
+            prev = main;
+            main = main->next;
+            ref = ref->next;
+        }
+
+        prev->next = main->next;
+        free(main);
+        main = NULL;
+    }
+}
+
+// Driver function
+int main (void)
+{
+    srand(time(0));
+
+    int num_vals = (rand() % 11) + 1;  
+    int k = rand() % num_vals + 1;      // generate a valid value for k
+
+    Node *head = init_rand_linked_list(num_vals);
+
+    print_linked_list(head);
+    printf("k = %d\n",k);
+    remove_kth_node_from_end(&head,k);
+    print_linked_list(head);
+
+    return 0;
+}
